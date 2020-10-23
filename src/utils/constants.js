d88fd83018e996680e3a1f70f88bac825cb45d67
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-10-23 00:06:43 -0300

    chore: install dependecies

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const BR_COUNTRY_CODE = 'BR';
+export const BR_LANG_CODE = 'pt_br';
+
+export const EN_COUNTRY_CODE = 'EN';
+export const EN_LANG_CODE = 'en';
---
+++ src/utils/urls.js
@@ -0,0 +1,3 @@
+export const API_URL = 'api.openweathermap.org/data/';
+export const API_VERSION = '2.5';
+export const OPEN_WEATHER_IMG_URL = 'http://openweathermap.org/img/wn/{iconCode}@2x.png';
